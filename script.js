645e16a6f9e5ed5808a88cf93946b7722d4e6ca4
Neil Shukla <neilshukla008@gmail.com>
2024-07-07 01:25:13 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+
+      // for tablet smooth
+      tablet: { smooth: true },
+
+      // for mobile
+      smartphone: { smooth: true }
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+          return arguments.length
+              ? locoScroll.scrollTo(value, 0, 0)
+              : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+          return {
+              top: 0,
+              left: 0,
+              width: window.innerWidth,
+              height: window.innerHeight
+          };
+      }
+
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+
+}
+
+
+
+
 var tl = gsap.timeline()
 tl.from("nav",{
     y:900,
@@ -43,3 +84,94 @@ Shery.textAnimate("p" /* Element to target.*/, {
   multiplier: 0.1,
 });
 
+function page3and4Animations() {
+
+  var elementContainer = document.querySelector("#element-container")
+
+  elementContainer.addEventListener("mouseenter", function () {
+      gsap.to("#moving-image", {
+          opacity: 1
+      })
+  })
+
+  elementContainer.addEventListener("mouseleave", function () {
+      gsap.to("#moving-image", {
+          opacity: 0
+      })
+  })
+
+  var allElements = document.querySelectorAll(".element")
+  var movingImageDiv = document.querySelector("#moving-image")
+  var moveImg = document.querySelector("#moving-image img")
+
+
+  allElements.forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+          let image = elem.getAttribute("data-image")
+          gsap.to(moveImg, {
+              attr: { src: image },
+          })
+      })
+      elementContainer.addEventListener("mousemove", function (dets) {
+          gsap.to("#moving-image", {
+              left: `${dets.x - elementContainer.getBoundingClientRect().x}`,
+              top: `${dets.y - elementContainer.getBoundingClientRect().y}`,
+              duration: 3,
+              ease: "power1.out"
+          })
+
+      })
+  })
+}
+
+
+
+function marqueAnimation() {
+
+  document.addEventListener("wheel", function (dets) {
+      if (dets.deltaY > 0) {
+          gsap.to("#move .marque", {
+              transform: "translateX(-200%)",
+              ease: "none",
+              repeat: -1,
+              duration: 5
+          })
+          gsap.to("#move .marque img", {
+              rotate: 180
+          })
+      } else {
+          gsap.to("#move .marque", {
+              transform: "translateX(0%)",
+              ease: "none",
+              repeat: -1,
+              duration: 5
+          })
+          gsap.to("#move .marque img", {
+              rotate: 0
+          })
+      }
+  })
+}
+
+
+
+// locomotiveAnimation()
+
+
+page3and4Animations()
+
+marqueAnimation()
+var roti = 0
+document.addEventListener("wheel",function(dets){
+  if(dets.deltaY>0){
+      roti += 10
+      gsap.to("#bottomright svg",{
+          transform:`translate(-50%,-50%) rotate(${roti}deg)`
+      })
+  }else{
+      roti -= 10
+      gsap.to("#bottomright svg",{
+          transform:`translate(-50%,-50%) rotate(${roti}deg)`
+      })
+  }
+})
